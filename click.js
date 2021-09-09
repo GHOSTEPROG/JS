9dced4d177d71b8daeda61932cd429eb9fd17bc1
IFTRYPROG <90372523+iftryprog@users.noreply.github.com>
2021-09-08 23:44:28 -0700

    Update click.js

--- click.js
+++ click.js
@@ -1,77 +1,42 @@
-<SCRIPT>
-
-
-document.onkeypress = function (event) {
-event = (event || window.event);
-if (event.keyCode == 123) {
-//alert('No F-12');
-return false;
-}
-}
-document.onmousedown = function (event) {
-event = (event || window.event);
-if (event.keyCode == 123) {
-//alert('No F-keys');
-return false;
-}
-}
-document.onkeydown = function (event) {
-event = (event || window.event);
-if (event.keyCode == 123) {
-//alert('No F-keys');
-return false;
-}
-}
-
-
-
-
-var message="Sorry, right-click has been disabled";
-
-function clickIE() {if (document.all) {(message);return false;}}
-function clickNS(e) {if
-(document.layers||(document.getElementById&&!document.all)) {
-if (e.which==2||e.which==3) {(message);return false;}}}
-if (document.layers)
-{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
-else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
-document.oncontextmenu=new Function("return false")
-//
-function disableCtrlKeyCombination(e)
-{
-//list all CTRL + key combinations you want to disable
-var forbiddenKeys = new Array('a', 'n', 'z', 'c', 'x', 'v', 'j' , 'w', 'u');
-var key;
-var isCtrl;
-if(window.event)
-{
-key = window.event.keyCode; //IE
-if(window.event.ctrlKey)
-isCtrl = true;
-else
-isCtrl = false;
-}
-else
-{
-key = e.which; //firefox
-if(e.ctrlKey)
-isCtrl = true;
-else
-isCtrl = false;
-}
-//if ctrl is pressed check if other key is in forbidenKeys array
-if(isCtrl)
-{
-for(i=0; i<forbiddenKeys.length; i++)
-{
-//case-insensitive comparation
-if(forbiddenKeys[i].toLowerCase() == String.fromCharCode(key).toLowerCase())
-{
-
-return false;
-}
-}
-}
-return true;
-}
+<script language="JavaScript">
+  /**
+    * Disable right-click of mouse, F12 key, and save key combinations on page
+    */
+  window.onload = function() {
+    document.addEventListener("contextmenu", function(e){
+      e.preventDefault();
+    }, false);
+    document.addEventListener("keydown", function(e) {
+    //document.onkeydown = function(e) {
+      // "I" key
+      if (e.ctrlKey &amp;&amp; e.shiftKey &amp;&amp; e.keyCode == ۷۳) {
+        disabledEvent(e);
+      }
+      // "J" key
+      if (e.ctrlKey &amp;&amp; e.shiftKey &amp;&amp; e.keyCode == ۷۴) {
+        disabledEvent(e);
+      }
+      // "S" key + macOS
+      if (e.keyCode == ۸۳ &amp;&amp; (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+        disabledEvent(e);
+      }
+      // "U" key
+      if (e.ctrlKey &amp;&amp; e.keyCode == ۸۵) {
+        disabledEvent(e);
+      }
+      // "F12" key
+      if (event.keyCode == ۱۲۳) {
+        disabledEvent(e);
+      }
+    }, false);
+    function disabledEvent(e){
+      if (e.stopPropagation){
+        e.stopPropagation();
+      } else if (window.event){
+        window.event.cancelBubble = true;
+      }
+      e.preventDefault();
+      return false;
+    }
+  };
 </script>
